7a984a9c9fdadc40018a5efc6e803b90cd405b62
bmaitner <bmaitner@gmail.com>
2017-05-01 10:41:59 -0700

    updated tarball with recent updates

--- BIEN/tests/testthat.R
+++ BIEN/tests/testthat.R
@@ -24,7 +24,7 @@ test_that("Metadata_citation function returns a list",{
 
 #Occurrence tests
 test_that("Occurrence functions return a dataframe",{
-  expect_that(BIEN_occurrence_box(min.lat = 32.9,max.lat = 33,min.long = -114,max.long = -113.9, cultivated = TRUE, only.new.world = FALSE),is_a("data.frame"))
+  expect_that(BIEN_occurrence_box(min.lat = 32.9,max.lat = 33,min.long = -114,max.long = -113.9, cultivated = TRUE, only.new.world = FALSE,all.taxonomy = T,native.status = T,natives.only = T,observation.type = T,political.boundaries = T),is_a("data.frame"))
   expect_that(BIEN_occurrence_country("Bahamas") ,is_a("data.frame"))
   expect_that(BIEN_occurrence_county(country = "United States",state = "Arizona",county = "Pima") ,is_a("data.frame"))
   expect_that(BIEN_occurrence_family(family = "Cactaceae",limit=1) ,is_a("data.frame"))
@@ -52,7 +52,7 @@ test_that("Plot functions return a dataframe",{
   expect_that(BIEN_plot_list_sampling_protocols(),is_a("data.frame"))
   expect_that(BIEN_plot_metadata() ,is_a("data.frame"))
   expect_that(BIEN_plot_name("test"),is_a("data.frame"))
-  expect_that(BIEN_plot_sampling_protocol(sampling_protocol = "test"),is_a("data.frame"))
+  expect_that(BIEN_plot_sampling_protocol(sampling_protocol = "test",natives.only = T),is_a("data.frame"))
   expect_that(BIEN_plot_state(country = "United States",state = "test"),is_a("data.frame"))
   })
 
@@ -63,7 +63,10 @@ test_that("Ranges functions return a SpatialPolygonsDataFrame",{
 
 #Stem
 test_that("Stem functions return a dataframe",{
-  expect_that(BIEN_stem_species(species = "Abies amabilis") ,is_a("data.frame"))
+  expect_that(BIEN_stem_species(species = "Abies amabilis",natives.only = T,native.status = T) ,is_a("data.frame"))
+  expect_that(BIEN_stem_genus(genus = "Abies",natives.only = T,native.status = T) ,is_a("data.frame"))
+  expect_that(BIEN_stem_family(family = "Selaginellaceae",natives.only = T,native.status = T) ,is_a("data.frame"))
+  
 })
 
 #Taxonomy
